2c73d918633311dbde9377a3a1fe796bc47446be
KBH <k01032633280@gmail.com>
2019-03-15 18:37:23 +0900

    going home

--- main_frame.cpp
+++ main_frame.cpp
@@ -11,25 +11,42 @@
 #include <stdio.h>
 #include <string>
 
+#include <vector>
 
+
+using namespace std;
 
 int main(int argc, char **argv)
 {
+
+  int i,j;
 
   App application(argc,argv);
 
   KWidget container;
   BorderLayout mainLayout;
 
+  KWidget side;
+  QVBoxLayout buttonLayout;
+  side.setLayout(&buttonLayout);
+
+
   KLabel *label = new KLabel("label");
-  KPushButton *button = new KPushButton("button");
   Switch *sw = new Switch("swtich");
+
+  vector<KPushButton*> buttons;
+
+  for(i=0;i<5;i++){
+    KPushButton *button = new KPushButton("button");
+    buttons.push_back(button);
+    buttonLayout.addWidget(button,BorderLayout::West);
+  }
   
   
   //label->setText("first line\nsecond line");
-
+ 
+  mainLayout.addWidget(&side,BorderLayout::West);
   mainLayout.addWidget(label,BorderLayout::Center);
-  mainLayout.addWidget(button,BorderLayout::West);
   mainLayout.addWidget(sw,BorderLayout::Center);
 
   container.setLayout(&mainLayout);
