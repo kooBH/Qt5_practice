60501c23bdc1261063032bb6f6599369f523a3fd
KBH <k01032633280@gmail.com>
2019-03-29 19:36:31 +0900

    Font

--- main_label.cpp
+++ main_label.cpp
@@ -1,4 +1,4 @@
-#include "App.h"
+#include <QApplication>
 #include "KLabel.h"
 #include "KWidget.h"
 
@@ -7,19 +7,31 @@
 #include <stdio.h>
 #include <string>
 
+#include <QTextCodec>
+#include <QFont>
 
 
 int main(int argc, char **argv)
 {
 
-  App application(argc,argv);
+  QApplication application(argc,argv);
 
   KWidget container;
   QVBoxLayout mainLayout;
   container.setLayout(&mainLayout);
 
-KLabel *label = new KLabel("label");
+  container.setFixedSize(500,500);
+
+   QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+   QFont font;
+   font.setFamily(codec->toUnicode("맑은 고딕"));
+   font.setPointSize(24);
+
+
+KLabel *label = new KLabel("맑은 고딕");
 //label->setText("first line\nsecond line");
+//
+  label->setFont(font);
 
   mainLayout.addWidget(label);
 
